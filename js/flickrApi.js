d53590c9a2b40e7a405c2930624a70f47f3a5dcd
Christa Favano <christa@greatprofessionalwebsites.com>
2015-12-17 15:48:29 -0800

    Testing commit with issue reference #1

--- js/flickrApi.js
+++ js/flickrApi.js
@@ -11,7 +11,6 @@ flickr.post('/', textParser, function(req,res) {
   request('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=c1a7eab619087ccae98ce36753b8c3f4&lat=' + latitude + '&lon=' + longtitude + '&radius=20&format=json&nojsoncallback=1&auth_token=72157661933492300-056ee478ad1e599a&api_sig=53db9696c6926a3c92568e93b6c21936', 
   function (error, response, body) {
     if (!error && response.statusCode == 200) {   
-      //parse into string
       var flickrImages = JSON.parse(body);  
       var data = JSON.stringify(flickrImages.photos.photo);
       res.send(data);
