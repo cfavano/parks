9fe7dc77921469ad8c7dc6e6891a1d5a1ac2c29a
Christa Favano <cvalencia0220@gmail.com>
2015-12-18 10:20:31 -0800

    Fixed flickr app call

--- js/flickrApi.js
+++ js/flickrApi.js
@@ -8,7 +8,7 @@ flickr.post('/', textParser, function(req,res) {
 //var keyword = req.body;
   var latitude = 44.454;
   var longtitude = -68.04902; 
-  request('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=c1a7eab619087ccae98ce36753b8c3f4&lat=' + latitude + '&lon=' + longtitude + '&radius=20&format=json&nojsoncallback=1&auth_token=72157661933492300-056ee478ad1e599a&api_sig=53db9696c6926a3c92568e93b6c21936', 
+  request('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=1d3cddfecb2b92af4908394508288a7c&lat='+ latitude +'&lon='+ longtitude +'&radius=20&format=json&nojsoncallback=1&auth_token=72157662403865471-2bc0838b9c68b4f5&api_sig=48d26697a8b1a592ca85555657520927', 
   function (error, response, body) {
     if (!error && response.statusCode == 200) {   
       var flickrImages = JSON.parse(body);  
