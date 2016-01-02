dddd883863a1492032924c50db916e3174b6e4a9
Christa Favano <cvalencia0220@gmail.com>
2016-01-02 00:41:31 -0800

    #1 Created a function to create images

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
 var submit = document.getElementById('location');
 
+function createImage(columnNumber, flickrPath){
+  var hero = document.getElementById('hero');
+  var placeholder = document.createElement('img');
+  placeholder.src = '../images/holder.png';
+  var imageContainer = document.createElement('div');
+  imageContainer.appendChild(placeholder);
+  var hero = document.getElementById('hero');
+  console.log(hero.appendChild(imageContainer));
+  imageContainer.setAttribute('class', 'col-sm-' + columnNumber + ' col-xs-3');
+  placeholder.setAttribute('class', 'img-responsive'); 
+  placeholder.style.backgroundImage  = "url('"+ flickrPath + "')";
+}
+
 function getFlickr(){  
   var index = document.getElementById('park');
   var parkName = index.options[index.selectedIndex].text;
@@ -9,40 +22,26 @@ function getFlickr(){
   var xhr = new XMLHttpRequest();
   xhr.open('POST', '/api/flickrImages', true);
   xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-  xhr.send(JSON.stringify({'nationalPark': parkName, 'latitude': latLong[0], 'longtitude': latLong[1] }));
+  xhr.send(JSON.stringify({'latitude': latLong[0], 'longtitude': latLong[1] }));
 
   xhr.onload = function() {
     if(xhr.status === 200) {
-      var heroContainer = document.querySelector('.hero');
+      var heroContainer = document.getElementById('hero');
       var firstChild = heroContainer.firstChild;
 
       while(firstChild){
         heroContainer.removeChild(firstChild);
         var firstChild = heroContainer.firstChild;
       }
-
+      
       var response = xhr.responseText;
       var responseText = JSON.parse(response);
       var data = responseText.photos.photo;
 
       if (data.length >= 36) {
         for(i = 0; i < 12; i++){
-          var hero = document.getElementsByClassName('hero')[0];
-          var placeholder = document.createElement('img');
-          placeholder.src = '../images/holder.png';
-          
-          var flickrImage = 'https://farm' + data[i*3].farm + '.staticflickr.com/' + data[i*3].server + '/' + data[i*3].id + '_' + data[i*3].secret + '_c.jpg';         
-          
-          var imageContainer = document.createElement('div');
-          imageContainer.appendChild(placeholder);
-
-          var hero = document.getElementsByClassName('hero')[0];
-          hero.appendChild(imageContainer);
-
-          imageContainer.setAttribute('class', 'col-sm-3 col-xs-3');
-          placeholder.setAttribute('class', 'img-responsive'); 
-
-          placeholder.style.backgroundImage  = "url('"+ flickrImage + "')";
+          var flickrImage = 'https://farm' + data[i*3].farm + '.staticflickr.com/' + data[i*3].server + '/' + data[i*3].id + '_' + data[i*3].secret + '_c.jpg';             
+          createImage(3, flickrImage);
         }
       }
   
@@ -50,68 +49,31 @@ function getFlickr(){
         var flickrBroad = new XMLHttpRequest(); 
         flickrBroad.open('POST', '/api/flickrImagesBroad', true);
         flickrBroad.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-        flickrBroad.send(JSON.stringify({'latitude': latLong[0], 'longtitude': latLong[1] }));
+        flickrBroad.send(JSON.stringify({'latitude': latLong[0], 'longtitude': latLong[1]}));
 
         flickrBroad.onload = function() {
-          if(flickrBroad.status === 200 ) {
+          if(flickrBroad.status === 200) {
             var responseFlickr = flickrBroad.responseText;
             var flickrText = JSON.parse(responseFlickr);
-            var flickrData = flickrText.photos.photo;
-            console.log(flickrData);
-            if (flickrData.length > 24) {
-              for(j = 0; j < 12; j++) {
-                var hero = document.getElementsByClassName('hero')[0];
-                var placeholder = document.createElement('img');
-                placeholder.src = '../images/holder.png';
-
-                var broadImage  = 'https://farm' + flickrData[j*2].farm + '.staticflickr.com/' + flickrData[j*2].server + '/' + flickrData[j*2].id + '_' + flickrData[j*2].secret + '_c.jpg';         
-                var imageContainer = document.createElement('div');
-                imageContainer.appendChild(placeholder);
-
-                var hero = document.getElementsByClassName('hero')[0];
-                hero.appendChild(imageContainer);
-
-                placeholder.setAttribute('class', 'img-responsive'); 
-
-                imageContainer.setAttribute('class', 'col-sm-3 col-xs-3');             
-                placeholder.style.backgroundImage  = "url('"+ broadImage + "')";
+            var data = flickrText.photos.photo;
+            
+            if (data.length >= 24) {
+              for(j = 0; j < 12; j++) {             
+                var flickrImage  = 'https://farm' + data[j*2].farm + '.staticflickr.com/' + data[j*2].server + '/' + data[j*2].id + '_' + data[j*2].secret + '_c.jpg';       
+                createImage(3, flickrImage);
               }
             }
        
-            else if (flickrData.length === 9){
+            else if (data.length === 9){
               for(j = 1; j < 9; j++) {
-                var hero = document.getElementsByClassName('hero')[0];
-                var placeholder = document.createElement('img');
-                placeholder.src = '../images/holder.png';
-
-                var broadImage  = 'https://farm' + flickrData[j].farm + '.staticflickr.com/' + flickrData[j].server + '/' + flickrData[j].id + '_' + flickrData[j].secret + '_c.jpg';         
-                var imageContainer = document.createElement('div');
-                imageContainer.appendChild(placeholder);
-
-                var hero = document.getElementsByClassName('hero')[0];
-                hero.appendChild(imageContainer);
-
-                placeholder.setAttribute('class', 'img-responsive'); 
-                imageContainer.setAttribute('class', 'col-sm-3 col-xs-4');             
-                placeholder.style.backgroundImage  = "url('"+ broadImage + "')";
+                var flickrImage  = 'https://farm' + data[j].farm + '.staticflickr.com/' + data[j].server + '/' + data[j].id + '_' + data[j].secret + '_c.jpg';         
+                createImage(3, flickrImage);
               }
             }
-            else if (flickrData.length === 2){
+            else if (data.length === 2){
               for(j = 0; j < 2; j++) {
-                var hero = document.getElementsByClassName('hero')[0];
-                var placeholder = document.createElement('img');
-                placeholder.src = '../images/holder.png';
-
-                var broadImage  = 'https://farm' + flickrData[j].farm + '.staticflickr.com/' + flickrData[j].server + '/' + flickrData[j].id + '_' + flickrData[j].secret + '_c.jpg';         
-                var imageContainer = document.createElement('div');
-                imageContainer.appendChild(placeholder);
-                imageContainer.setAttribute('class', 'col-sm-6 col-xs-6');             
-                placeholder.style.backgroundImage  = "url('"+ broadImage + "')";
-
-                var hero = document.getElementsByClassName('hero')[0];
-                hero.appendChild(imageContainer);
-
-                placeholder.setAttribute('class', 'img-responsive'); 
+                var flickrImage  = 'https://farm' + data[j].farm + '.staticflickr.com/' + data[j].server + '/' + data[j].id + '_' + data[j].secret + '_c.jpg';         
+                createImage(6, flickrImage);
               }
             }
           }
@@ -132,7 +94,6 @@ function getRecreation(){
   
   xhr.onload = function() {
     if(xhr.status === 200) {
-
       var response = xhr.responseText;
       var responseText = JSON.parse(response);
       var facilityId = JSON.stringify(responseText.ID);
